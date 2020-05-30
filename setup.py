cb46ab858d5945d8d5a4d29da4ed7b8d79571cc5
Fedor Soldatkin <fsoldatkin@yandex.ru>
2020-05-30 17:04:50 +0300

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='log_analyzer',
+    version='1.0',
+    py_modules=['log_analyzer'],
+    url='',
+    license='MIT License',
+    author='Fedor Soldatkin',
+    author_email='fsoldatkin@yandex.ru',
+    description='Nginx logs simple analyzer.'
+)
